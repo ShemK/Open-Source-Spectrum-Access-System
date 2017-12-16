73231366ab13621f2fbfa641c4a743ff4033dba7
ShemK <shemk@vt.edu>
2017-12-15 21:13:32 -0500

    Added new test files

---
+++ loop.cpp
@@ -0,0 +1,96 @@
+#include "loop.hpp"
+
+Loop::Loop()
+{
+  sem_init(&mutex,0,1);
+
+  fd_shm = shm_open("/shared_loop", O_CREAT | O_RDWR, 0777);
+  if (fd_shm == -1)
+  {
+    perror("failed to create shared memory\n");
+  }
+  int status = ftruncate(fd_shm, sizeof(shared_struct));
+  if (status == -1)
+  {
+    perror("Error Occured while truncating memory\n");
+  }
+
+  shared = (shared_struct *)mmap(NULL, sizeof(shared_struct), PROT_READ | PROT_WRITE, MAP_SHARED, fd_shm, 0);
+
+}
+
+Loop::~Loop()
+{
+
+}
+
+
+void Loop::transmit(std::vector<std::complex<float>> usrp_buffer,int buffer_len){
+  //char *frame = new char[MAX_BUF];
+  //int frame_len = buffer_len*sizeof(std::complex<float>);
+  int sval;
+  sem_getvalue(&mutex, &sval);
+  //printf("Current TX semaphore Value: %d\n", sval);
+  sem_wait(&mutex);
+  shared->num_samples = buffer_len;
+  memcpy(&shared->data[0],&usrp_buffer[0],buffer_len*sizeof(std::complex<float>));
+  /*
+  for(int i = 0; i < buffer_len; i++){
+    shared->data[i] = usrp_buffer[i];
+  }
+  */
+  //sem_post(&mutex);
+  //delete[] frame;
+}
+
+int Loop::receive(std::vector<std::complex<float> > &recv){
+  //sem_wait(&mutex);
+
+  //if(sem_trywait(&mutex) == 0){
+    int sval;
+    sem_getvalue(&mutex, &sval);
+    //printf("Current RX semaphore Value: %d\n", sval);
+    if(shared->data[0] != std::complex<float>(1000, -1000)){
+      memcpy(&recv[0],&shared->data[0],shared->num_samples*sizeof(std::complex<float>));
+    } else{
+      return 0;
+    }
+    shared->data[0] = std::complex<float>(1000, -1000);
+    if(sval < 1){
+      sem_post(&mutex);
+    } 
+    return shared->num_samples;
+  //} else{
+  //  return 0;
+  //}
+
+}
+
+void Loop::manipulate_header(char *payload){
+  //printf("\n----------------Manipulating data-------------------\n");
+  // mac address change
+  char temp = payload[23];
+  payload[23] =  payload[17];
+  payload[17] = temp;
+  //  printf("%02x --- %02x\n",payload[17] ,payload[23]);
+  // ipaddress change
+
+  temp = payload[45];
+  payload[45] =  payload[41];
+  payload[41] = temp;
+  //  printf("%02x --- %02x\n",payload[41] ,payload[45]);
+  /*
+  for(int i = 0; i < 44; i++){
+  printf("%02x ",payload[i]);
+}
+printf("\n");
+printf("\n----------------Done Manipulating data-------------------\n");
+*/
+  temp = payload[48];
+  payload[48] =  payload[46];
+  payload[46] = temp;
+
+  temp = payload[49];
+  payload[49] =  payload[47];
+  payload[47] = temp;
+}
---
+++ loop.hpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <mqueue.h>
+#include <string.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <time.h>
+#include <sys/time.h>
+#include <errno.h>
+#include <arpa/inet.h>
+#include <iomanip>
+#include <zlib.h>
+#include <sys/ioctl.h>
+#include <linux/if_tun.h>
+#include <netinet/if_ether.h>
+#include <queue>
+#include <map>
+#include <future>
+#include <functional>
+#include <cstring>
+#include <bitset>
+#include <complex>
+#include <semaphore.h>
+#include <sys/mman.h>
+
+#define RED "\x1B[31m"
+#define GRN "\x1B[32m"
+#define YEL "\x1B[33m"
+#define BLU "\x1B[34m"
+#define MAG "\x1B[35m"
+#define CYN "\x1B[36m"
+#define WHT "\x1B[37m"
+#define RESET "\x1B[0m"
+
+typedef unsigned short uint16;
+
+#define MAX_SHARED_BUFFER 300000
+#define PMODE 0777
+
+class Loop
+{
+private:
+public:
+  Loop();
+  ~Loop();
+  sem_t mutex;
+
+  int fd_shm;
+
+  struct shared_struct{
+    int sample = 0;
+    double frequency = 0;
+    double sample_rate = 0;
+    int num_samples = 0;
+    std::complex<float> data[MAX_SHARED_BUFFER];
+  };
+
+  shared_struct *shared;
+
+  void transmit(std::vector<std::complex<float>> usrp_buffer,int buffer_len);
+  int receive(std::vector<std::complex<float> > &recv);
+  void manipulate_header(char *payload);
+};
