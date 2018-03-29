a6a03613a01f1525db544187b1fc53d581b29ba5
ShemK <skikamaze@gmail.com>
2018-03-29 18:29:14 -0400

    Adding options to install.sh

--- services/install.sh
+++ services/install.sh
@@ -1,240 +1,287 @@
 #!/bin/bash
 
-echo "Installing REM"
 
-echo "Installing pip"
+install_dependencies(){
+  echo "Installing REM"
 
-sudo apt-get -y install python-pip
+  echo "Installing pip"
 
-echo "Python-pip installation complete"
+  sudo apt-get -y install python-pip
 
-#sudo pip install -U pandas
+  echo "Python-pip installation complete"
 
-sudo chmod -R 755 /usr/local/lib/python2.7/dist-packages/*
+  #sudo pip install -U pandas
 
-echo "Dependency installation complete"
+  sudo chmod -R 755 /usr/local/lib/python2.7/dist-packages/*
 
-echo "Installing PostGre SQL"
+  echo "Dependency installation complete"
 
-sudo apt-get install -y postgresql postgresql-contrib
+  echo "Installing PostGre SQL"
 
-echo "Creating user - wireless with password wireless"
+  sudo apt-get install -y postgresql postgresql-contrib
 
-echo "-------------------------------------------------------"
+  echo "Creating user - wireless with password wireless"
 
-echo "Password can be changed with the following command:"
+  echo "-------------------------------------------------------"
 
-echo "sudo -i -u postgres psql -c \"ALTER USER wireless WITH PASSWORD 'wireless';\""
+  echo "Password can be changed with the following command:"
 
-echo "-------------------------------------------------------"
+  echo "sudo -i -u postgres psql -c \"ALTER USER wireless WITH PASSWORD 'wireless';\""
 
-sudo -i -u postgres psql -c"CREATE USER wireless WITH PASSWORD 'wireless';"
+  echo "-------------------------------------------------------"
 
-sudo -i -u postgres psql -c"ALTER USER wireless WITH SUPERUSER;"
+  sudo -i -u postgres psql -c"CREATE USER wireless WITH PASSWORD 'wireless';"
 
-sudo service postgresql restart
+  sudo -i -u postgres psql -c"ALTER USER wireless WITH SUPERUSER;"
 
-sudo -i -u postgres psql -c"DROP DATABASE IF EXISTS rem;"
+  sudo service postgresql restart
 
-sudo -i -u postgres psql -c"CREATE DATABASE rem;"
+  sudo -i -u postgres psql -c"DROP DATABASE IF EXISTS rem;"
 
-
-
-echo "Installing libpqxx-dev for C++ psql connector"
+  sudo -i -u postgres psql -c"CREATE DATABASE rem;"
 
 
-if [[ `lsb_release -rs` == "14.04" ]]
-then
-sudo apt-get -y install libpqxx3-dev
-sudo apt-get -y install pgadmin3
-fi
+
+  echo "Installing libpqxx-dev for C++ psql connector"
 
 
-if [[ `lsb_release -rs` == "16.04" ]]
-then
-sudo apt-get -y install libpqxx-dev
-sudo apt-get -y install pgadmin3
-fi
+  if [[ `lsb_release -rs` == "14.04" ]]
+  then
+  sudo apt-get -y install libpqxx3-dev
+  sudo apt-get -y install pgadmin3
+  fi
 
-echo "Installing Dependencies - Going to take a long time"
 
-sudo apt-get install -y libblas-dev liblapack-dev libatlas-base-dev gfortran
+  if [[ `lsb_release -rs` == "16.04" ]]
+  then
+  sudo apt-get -y install libpqxx-dev
+  sudo apt-get -y install pgadmin3
+  fi
 
-sudo pip install scipy pynmea2 numpy psycopg2 pandas plotly libconf
+  echo "Installing Dependencies - Going to take a long time"
 
-sudo chmod -R 755 /usr/local/lib/python2.7/dist-packages/*
+  sudo apt-get install -y libblas-dev liblapack-dev libatlas-base-dev gfortran
 
-echo "--------------------------------------------------------------"
-echo "Cloning rem branch from github repository"
-echo "--------------------------------------------------------------"
+  sudo pip install scipy pynmea2 numpy psycopg2 pandas plotly libconf
 
-git clone -b rem https://github.com/ShemK/Open-Source-Spectrum-Access-System rem
+  sudo chmod -R 755 /usr/local/lib/python2.7/dist-packages/*
+}
 
-echo "--------------------------------------------------------------"
-echo "Installing GNURadio Dependencies for REM"
-echo "--------------------------------------------------------------"
-#<<COMMENT
-sudo rm -r rem/gr-sas/build
+install_sensor_component(){
+  echo "--------------------------------------------------------------"
+  echo "Cloning rem branch from github repository"
+  echo "--------------------------------------------------------------"
 
-mkdir rem/gr-sas/build
+  git clone -b rem https://github.com/ShemK/Open-Source-Spectrum-Access-System rem
 
-cd rem/gr-sas/build
+  echo "--------------------------------------------------------------"
+  echo "Installing GNURadio Dependencies for REM"
+  echo "--------------------------------------------------------------"
+  #<<COMMENT
+  sudo rm -r rem/gr-sas/build
 
-cmake ..
+  mkdir rem/gr-sas/build
 
-make -j8
+  cd rem/gr-sas/build
 
-sudo make install
+  cmake ..
 
-sudo ldconfig
+  make -j8
 
-cd ../../../
+  sudo make install
 
-sudo rm -r rem/gr-utils/build
+  sudo ldconfig
 
-mkdir rem/gr-utils/build
+  cd ../../../
 
-cd rem/gr-utils/build
+  sudo rm -r rem/gr-utils/build
 
-cmake ..
+  mkdir rem/gr-utils/build
 
-make -j8
+  cd rem/gr-utils/build
 
-sudo make install
+  cmake ..
 
-sudo ldconfig
+  make -j8
 
-cd ../../../
+  sudo make install
+
+  sudo ldconfig
+
+  cd ../../../
+}
+
 #COMMENT
+install_server(){
+  echo "Populating database"
 
-echo "Populating database"
+  psql -h localhost -U wireless rem < rem/sql/shem_rem.sql
 
-psql -h localhost -U wireless rem < rem/sql/shem_rem.sql
+  echo "---------------------------------------------"
+  echo "Installing Apache Web Server"
+  echo "---------------------------------------------"
 
-echo "---------------------------------------------"
-echo "Installing Apache Web Server"
-echo "---------------------------------------------"
+  if [[ `lsb_release -rs` == "14.04" ]]
+  then
+  sudo apt-get -y install apache2
+  sudo apt-get -y install php5
+  sudo apt-get -y install libapache2-mod-php5
+  sudo apt-get -y install php5-pgsql
+  sudo service apache2 restart
+  fi
 
-if [[ `lsb_release -rs` == "14.04" ]]
-then
-sudo apt-get -y install apache2
-sudo apt-get -y install php5
-sudo apt-get -y install libapache2-mod-php5
-sudo apt-get -y install php5-pgsql
-sudo service apache2 restart
-fi
+  echo "--------------------------------------------------------------"
+  echo "Downloading Server Files"
+  echo "--------------------------------------------------------------"
 
-echo "--------------------------------------------------------------"
-echo "Downloading Server Files"
-echo "--------------------------------------------------------------"
+  sudo rm -r /var/www/html/spectrumAccessSystem
 
-sudo rm -r /var/www/html/spectrumAccessSystem
+  sudo rm -r spectrumAccessSystem
 
-sudo rm -r spectrumAccessSystem
+  git clone -b sas_php https://github.com/ShemK/Open-Source-Spectrum-Access-System spectrumAccessSystem
 
-git clone -b sas_php https://github.com/ShemK/Open-Source-Spectrum-Access-System spectrumAccessSystem
+  sudo mv spectrumAccessSystem/server /var/www/html/spectrumAccessSystem
 
-sudo mv spectrumAccessSystem/server /var/www/html/spectrumAccessSystem
-
-sudo chmod 644 /var/www/html/spectrumAccessSystem/*
-
-
-echo "--------------------------------------------------------------"
-echo "Downloading and installing GPSD"
-echo "--------------------------------------------------------------"
-
-#install scons
-sudo apt-get -y install scons
-
-#cloning gpsd repository
-git clone https://git.savannah.nongnu.org/git/gpsd.git
+  sudo chmod 644 /var/www/html/spectrumAccessSystem/*
 
 
-#install gpsd
-cd gpsd && sudo scons && sudo scons udev-install
-
-sudo ldconfig
-
-cd ..
-
-sudo chmod -R 755 /usr/lib/python2.7/dist-packages/gps
-
-# make libconf files accessible
-sudo chmod  755 /usr/local/lib/python2.7/dist-packages/libc*
-
-ip_address=$(ip -4 addr show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
-
-node_id=$(echo $ip_address | cut -d . -f 4)
-node_id=$(bc -l <<< "$node_id * 100.0")
-
-#node_id=5000.1
-
-echo $node_id
-echo "--------------------------------------------------------------"
-echo "Downloading and installing service files"
-echo "--------------------------------------------------------------"
+}
 
 
-sudo rm -r /opt/sas
+install_gps(){
+  echo "--------------------------------------------------------------"
+  echo "Downloading and installing GPSD"
+  echo "--------------------------------------------------------------"
 
-sudo mkdir /opt/sas
+  #install scons
+  sudo apt-get -y install scons
 
-sudo chmod 665 /opt/sas
-
-sudo cp rem/apps/top_block.py /opt/sas/
-
-sudo cp rem/apps/sas_rem.py /opt/sas/
-
-sudo chmod 665 /opt/sas/*
+  #cloning gpsd repository
+  git clone https://git.savannah.nongnu.org/git/gpsd.git
 
 
-echo "--------------------------------------------------------------"
-echo "Creating fake gps file"
-echo "--------------------------------------------------------------"
+  #install gpsd
+  cd gpsd && sudo scons && sudo scons udev-install
 
-echo "\$GPRMC,125412.48,A,$node_id,N,$node_id,E,00.0,000.0,230506,00,E" > fake.log
+  sudo ldconfig
 
-sudo mkdir /opt/sas/gps/
+  cd ..
 
-sudo mv fake.log /opt/sas/gps/
+  sudo chmod -R 755 /usr/lib/python2.7/dist-packages/gps
 
-sudo chmod 665 /opt/sas/gps
+  # make libconf files accessible
+  sudo chmod  755 /usr/local/lib/python2.7/dist-packages/libc*
 
-sudo chmod 664 /opt/sas/gps/*
+  ip_address=$(ip -4 addr show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
 
-sudo rm -r auto_scripts
+  node_id=$(echo $ip_address | cut -d . -f 4)
+  node_id=$(bc -l <<< "$node_id * 100.0")
 
-git clone -b auto_scripts https://github.com/ShemK/Open-Source-Spectrum-Access-System auto_scripts
+  #node_id=5000.1
+  echo $node_id
+}
 
-sudo cp auto_scripts/services/channel_analysis.service /etc/systemd/system/
+install_services(){
+  echo "--------------------------------------------------------------"
+  echo "Downloading and installing service files"
+  echo "--------------------------------------------------------------"
 
-sudo chmod 664 /etc/systemd/system/channel_analysis.service
 
-sudo cp auto_scripts/services/sdr_phy.service /etc/systemd/system/
+  sudo rm -r /opt/sas
 
-sudo chmod 664 /etc/systemd/system/sdr_phy.service
+  sudo mkdir /opt/sas
 
-sudo cp auto_scripts/services/fakegps.service /etc/systemd/system/
+  sudo chmod 665 /opt/sas
 
-sudo chmod 664 /etc/systemd/system/fakegps.service
+  sudo cp rem/apps/top_block.py /opt/sas/
 
-sudo cp auto_scripts/services/cbsd_start.service /etc/systemd/system/
+  sudo cp rem/apps/sas_rem.py /opt/sas/
 
-sudo chmod 664 /etc/systemd/system/cbsd_start.service
+  sudo chmod 665 /opt/sas/*
 
-sudo systemctl daemon-reload
 
-sudo systemctl enable channel_analysis.service
+  echo "--------------------------------------------------------------"
+  echo "Creating fake gps file"
+  echo "--------------------------------------------------------------"
 
-sudo systemctl enable fakegps.service
+  echo "\$GPRMC,125412.48,A,$node_id,N,$node_id,E,00.0,000.0,230506,00,E" > fake.log
 
-sudo systemctl restart fakegps.service
+  sudo mkdir /opt/sas/gps/
 
-sudo cp auto_scripts/services/sas_sudoers /etc/sudoers.d/sas
+  sudo mv fake.log /opt/sas/gps/
 
-sudo chmod 440 /etc/sudoers.d/sas
+  sudo chmod 665 /opt/sas/gps
 
-cd
+  sudo chmod 664 /opt/sas/gps/*
+
+  sudo rm -r auto_scripts
+
+  git clone -b auto_scripts https://github.com/ShemK/Open-Source-Spectrum-Access-System auto_scripts
+
+  sudo cp auto_scripts/services/channel_analysis.service /etc/systemd/system/
+
+  sudo chmod 664 /etc/systemd/system/channel_analysis.service
+
+  sudo cp auto_scripts/services/sdr_phy.service /etc/systemd/system/
+
+  sudo chmod 664 /etc/systemd/system/sdr_phy.service
+
+  sudo cp auto_scripts/services/fakegps.service /etc/systemd/system/
+
+  sudo chmod 664 /etc/systemd/system/fakegps.service
+
+  sudo cp auto_scripts/services/cbsd_start.service /etc/systemd/system/
+
+  sudo chmod 664 /etc/systemd/system/cbsd_start.service
+
+  sudo systemctl daemon-reload
+
+  sudo systemctl enable channel_analysis.service
+
+  sudo systemctl enable fakegps.service
+
+  sudo systemctl restart fakegps.service
+
+  sudo cp auto_scripts/services/sas_sudoers /etc/sudoers.d/sas
+
+  sudo chmod 440 /etc/sudoers.d/sas
+
+  cd
+}
+while getopts dsgv option;
+do
+  case "$option"
+    in
+    d)
+      install_dependencies
+    ;;
+    s)
+      install_sensor_component
+    ;;
+
+    g)
+      install_gps
+    ;;
+
+    v)
+      install_services
+    ;;
+
+    e)
+      install_server
+    ;;
+    *)
+      install_dependencies
+      install_sensor_component
+      install_server
+      install_gps
+      install_services
+    ;;
+  esac
+
+done
+
+
 # installing the central controllers
 sudo rm -r central_controller
 
